370282eefbccbf5ffd8585f5448fd175d00b46bb
leszczynskim <mikozp@gmail.com>
2016-05-11 21:03:51 +0200

    Tree

--- MFCApplication5/MFCApplication5Dlg.cpp
+++ MFCApplication5/MFCApplication5Dlg.cpp
@@ -21,9 +21,11 @@ using namespace std;
 using namespace boost::filesystem;
 
 std::vector<CString> ItemsNames;
-std::map<CString,CString> Items;
+std::map<CString, CString> Items;
 CString last;
 int index = 0;
+LPCTSTR m_strFolderPath;
+
 
 CMFCApplication5Dlg::CMFCApplication5Dlg(CWnd* pParent /*=NULL*/)
 	: CDialogEx(CMFCApplication5Dlg::IDD, pParent)
@@ -43,7 +45,7 @@ BEGIN_MESSAGE_MAP(CMFCApplication5Dlg, CDialogEx)
 	ON_WM_QUERYDRAGICON()
 	ON_LBN_SELCHANGE(IDC_LIST1, &CMFCApplication5Dlg::OnLbnSelchangeList1)
 	ON_BN_CLICKED(IDC_BUTTON1, &CMFCApplication5Dlg::OnBnClickedButton1)
-	//ON_NOTIFY(TVN_ITEMCHANGED, 0x1221, &CMFCApplication5Dlg::OnTvnItemChangedTree1)
+	ON_NOTIFY(TVN_SELCHANGED, 0x1221, &CMFCApplication5Dlg::OnTvnItemChangedTree1)
 END_MESSAGE_MAP()
 
 
@@ -58,12 +60,11 @@ BOOL CMFCApplication5Dlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 
-    m_tree.Create(WS_CHILD | WS_VISIBLE | WS_BORDER | WS_TABSTOP |
-		             TVS_HASLINES | TVS_HASBUTTONS | TVS_LINESATROOT |
-			 TVS_SINGLEEXPAND | TVS_SHOWSELALWAYS |  
-			 TVS_TRACKSELECT,
-		             CRect(690, 10, 1005, 500), this, 0x1221);
-	
+	m_tree.Create(WS_CHILD | WS_VISIBLE | WS_BORDER | WS_TABSTOP |
+		TVS_HASLINES | TVS_HASBUTTONS | TVS_LINESATROOT |
+		TVS_SHOWSELALWAYS | TVS_TRACKSELECT,
+		CRect(690, 10, 1005, 500), this, 0x1221);
+
 	// TODO: Add extra initialization here
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
@@ -103,23 +104,24 @@ HCURSOR CMFCApplication5Dlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
-bool hasEnding (std::string const &fullString, std::string const &ending) {
-    if (fullString.length() >= ending.length()) {
-        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
-    } else {
-        return false;
-    }
+bool hasEnding(std::string const &fullString, std::string const &ending) {
+	if (fullString.length() >= ending.length()) {
+		return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
+	}
+	else {
+		return false;
+	}
 }
 
 void CMFCApplication5Dlg::Recurse(LPCTSTR pstr)
 {
-   CFileFind finder;
-   CString strWildcard(pstr);
-   strWildcard += _T("\\*.ITD");
-   BOOL bWorking, isITD = finder.FindFile(strWildcard);
-   
+	CFileFind finder;
+	CString strWildcard(pstr);
+	strWildcard += _T("\\*.ITD");
+	BOOL bWorking, isITD = finder.FindFile(strWildcard);
+
 	HTREEITEM hTree = NULL;
-	if(!isITD)
+	if (!isITD)
 	{
 		strWildcard = pstr;
 		strWildcard += _T("\\*.*");
@@ -132,59 +134,59 @@ void CMFCApplication5Dlg::Recurse(LPCTSTR pstr)
 				continue;
 
 			CString ext = finder.GetFileName().Right(3);
-			if(ext == "bmp" && !last.IsEmpty())
+			if (ext == "bmp" && !last.IsEmpty())
 			{
 				CString tmp = last + ":" + finder.GetFileTitle();
-				Items.insert(pair<CString,CString>(tmp, finder.GetFilePath()));
-				m_tree.InsertItem(tmp,hTree);
-				
+				Items.insert(pair<CString, CString>(tmp, finder.GetFilePath()));
+				m_tree.InsertItem(tmp, hTree);
+
 			}
 			if (finder.IsDirectory())
 			{
-				hTree = m_tree.InsertItem(finder.GetFileTitle(),TVI_ROOT);
+				hTree = m_tree.InsertItem(finder.GetFileTitle(), TVI_ROOT);
 				CString str = finder.GetFilePath();
 				Recurse(str);
 			}
 		}
 	}
-   while (bWorking)
-   {
-      bWorking = finder.FindNextFile();
-	  if (finder.IsDots())
-         continue;
-	  last = finder.GetFileTitle();
-   }
+	while (bWorking)
+	{
+		bWorking = finder.FindNextFile();
+		if (finder.IsDots())
+			continue;
+		last = finder.GetFileTitle();
+	}
 
 
-   
-   finder.Close();
+
+	finder.Close();
 }
 
-void CMFCApplication5Dlg::LoadFiles( const path & dir_path, HTREEITEM *root, bool *isFound )
+void CMFCApplication5Dlg::LoadFiles(const path & dir_path, HTREEITEM *root, bool *isFound)
 {
-	if ( !exists( dir_path ) ) 
+	if (!exists(dir_path))
 		return;
 	directory_iterator end_itr;
 	bool localIsFound = false;
-	for ( directory_iterator itr( dir_path ); itr != end_itr; ++itr )
+	for (directory_iterator itr(dir_path); itr != end_itr; ++itr)
 	{
-		if ( is_directory(itr->status()) )
+		if (is_directory(itr->status()))
 		{
 			*isFound = false;
 			HTREEITEM rootTmp = NULL;
-			if(!root)
+			if (!root)
 				rootTmp = m_tree.InsertItem(itr->path().filename().c_str(), TVI_ROOT);
 			else
 				rootTmp = m_tree.InsertItem(itr->path().filename().c_str(), *root);
-			LoadFiles( itr->path(), &rootTmp, isFound);
+			LoadFiles(itr->path(), &rootTmp, isFound);
 			if (!(*isFound))
 			{
 				m_tree.DeleteItem(rootTmp);
-				if(localIsFound)
+				if (localIsFound)
 					*isFound = true;
 			}
 		}
-		if( itr->path().extension() == ".bmp" )
+		if (itr->path().extension() == ".bmp")
 		{
 			localIsFound = true;
 			*isFound = true;
@@ -199,37 +201,75 @@ void CMFCApplication5Dlg::LoadFiles( const path & dir_path, HTREEITEM *root, boo
 void CMFCApplication5Dlg::OnLbnSelchangeList1()
 {
 	//int i = m_Values.GetCaretIndex();
-	int i =0;
+	int i = 0;
 	CString tmp = Items[ItemsNames[i]];
-	HBITMAP startBitmap = (HBITMAP)LoadImage(NULL, tmp, IMAGE_BITMAP,0,0, LR_LOADFROMFILE);
+	HBITMAP startBitmap = (HBITMAP)LoadImage(NULL, tmp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 	m_PictureControl.SetBitmap(startBitmap);
 	index = 0;
 }
 
 void CMFCApplication5Dlg::OnBnClickedButton1()
 {
-	CFolderDialog dlg( _T( "Root folder is C:\ " ), NULL, this );
-    dlg.SetRootFolder( _T( "C:\input_do_ineksera" ));
-	LPCTSTR m_strFolderPath;
+	CFolderDialog dlg(_T("Root folder is C:\ "), NULL, this);
+	dlg.SetRootFolder(_T("C:\input_do_ineksera"));
 	LPCTSTR m_strDisplayName;
-    if( dlg.DoModal() == IDOK  )
-    {    
-		m_strFolderPath = dlg.GetFolderPath();
-		bool b = false;
-		LoadFiles(m_strFolderPath, NULL, &b);
-    }
+	m_strFolderPath = L"C:\\input_do_ineksera";
+	bool b = false;
+	LoadFiles(m_strFolderPath, NULL, &b);
+	/*if( dlg.DoModal() == IDOK  )
+	{
+	m_strFolderPath = dlg.GetFolderPath();
+	LoadFiles(m_strFolderPath, NULL, &b);
+	}*/
 }
 
 void CMFCApplication5Dlg::OnTvnItemChangedTree1(NMHDR *pNMHDR, LRESULT *pResult)
 {
 	NMTVITEMCHANGE *pNMTVItemChange = reinterpret_cast<NMTVITEMCHANGE*>(pNMHDR);
 	// TODO: Add your control notification handler code here
+	wstring finalString;// = m_strFolderPath;
 	HTREEITEM a = m_tree.GetSelectedItem();
+	CString s = m_tree.GetItemText(a);
+	finalString += LPCTSTR("\\");
+	finalString += (LPCTSTR)s;
+
 	int i = 0;
-	while (m_tree.GetParentItem(a) == nullptr)
+	while (a != NULL)
 	{
 		a = m_tree.GetParentItem(a);
+		s = m_tree.GetItemText(a);
+		auto tmp = finalString;
+		finalString = LPCTSTR("\\") + s;
+		finalString += tmp;
 		i++;
 	}
-	*pResult = 0;
+	auto tmp = finalString;
+	finalString.clear();
+	finalString = CString(m_strFolderPath);// m_strFolderPath;
+	finalString += tmp;
+	CBitmap m_bitmap;
+	HBITMAP startBitmap = (HBITMAP)LoadImage(NULL, finalString.c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	if (startBitmap != NULL)
+	{
+		//CBitmap bitmap;
+		//CRect *rect = new CRect(0, 0, 500, 300);
+
+		//m_PictureControl.GetClientRect(rect);
+
+		//bitmap.Attach(startBitmap);
+
+		//m_PictureControl.SetBitmap(bitmap);
+
+		//// Now it would be supposed to fit the control:
+		//CDC *pDC = m_PictureControl.GetDC();
+		//CDC m_DC;
+
+		//m_DC.CreateCompatibleDC(pDC);
+		//m_DC.SelectObject(&bitmap);
+
+		//pDC->StretchBlt(0, 0, rect->Width(), rect->Height(), &m_DC, 0, 0, 67, 37, SRCCOPY);
+
+		m_bitmap.Attach(startBitmap);
+		m_PictureControl.SetBitmap(m_bitmap);
+	}
 }
